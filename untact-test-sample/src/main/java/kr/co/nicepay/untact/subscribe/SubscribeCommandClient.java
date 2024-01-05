c23e460cd52dfe32f91e1fb9a7386477e4f4d2a7
SeokRae <kslbsh@gmail.com>
2024-01-05 10:15:11 +0900

    - [x] web client 로직 리펙토링

--- untact-test-sample/src/main/java/kr/co/nicepay/untact/subscribe/SubscribeCommandClient.java
+++ untact-test-sample/src/main/java/kr/co/nicepay/untact/subscribe/SubscribeCommandClient.java
@@ -19,6 +19,7 @@ import org.springframework.web.client.ResourceAccessException;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.function.Supplier;
 
 @Slf4j
 @Service
@@ -34,110 +35,35 @@ public class SubscribeCommandClient implements SubscribeOutboundPort {
     private final JsonHelper mapper;
 
     @Override
-    public SubscribeCreatePgRes issue(
-            final SubscribeCreatePgReq subscribeCreatePgReq
-    ) {
-
+    public SubscribeCreatePgRes issue(final SubscribeCreatePgReq subscribeCreatePgReq) {
         log.info("SubscribeRestTempleUseCase.issue : {}", subscribeCreatePgReq);
-
-        try {
-            ResponseEntity<SubscribeCreatePgRes> issuedSubscribeKey = subscribeHttpRequest(
-                    subscribeUrlProperties.getIssue()
-                    , new HashMap<>()
-                    , subscribeCreatePgReq
-                    , SubscribeCreatePgRes.class
-            );
-            return issuedSubscribeKey.getBody();
-
-        } catch (HttpClientErrorException e) {
-            log.error("Subscribe Rest OutBound Client Exception issue: {}", e.getMessage());
-            throw new HttpClientErrorException(e.getStatusCode(), "Client Exception");
-        } catch (HttpServerErrorException e) {
-            log.error("Subscribe Rest OutBound Server Exception issue: {}", e.getMessage());
-            throw new HttpServerErrorException(e.getStatusCode(), "Server Exception");
-        } catch (ResourceAccessException e) {
-            log.error("Subscribe Rest OutBound Resource Access Exception issue: {}", e.getMessage());
-            throw new ResourceAccessException("Subscribe Rest OutBound Resource Access Exception");
-        } catch (RuntimeException e) {
-            log.error("Subscribe Rest OutBound Runtime Exception issue: {}", e.getMessage());
-            throw new RuntimeException("Subscribe Rest OutBound Runtime Exception");
-        } catch (Exception e) {
-            log.error("Subscribe Rest OutBound Exception issue: {}", e.getMessage());
-            throw new RuntimeException("Subscribe Rest OutBound Exception");
-        }
+        return handleRequest(() -> subscribeHttpRequest(
+                subscribeUrlProperties.getIssue(),
+                new HashMap<>(),
+                subscribeCreatePgReq,
+                SubscribeCreatePgRes.class));
     }
 
     @Override
-    public PaymentsRes payments(
-            final Id<String> bid,
-            final SubscribePaymentsReq subscribePaymentsReq
-    ) {
+    public PaymentsRes payments(final Id<String> bid, final SubscribePaymentsReq subscribePaymentsReq) {
         log.info("SubscribeRestTempleUseCase.processPayment");
-
         Map<String, Object> uriVariables = createUriVariables(bid);
-
-        try {
-            ResponseEntity<PaymentsRes> response = subscribeHttpRequest(
-                    subscribeUrlProperties.getPayments()
-                    , uriVariables
-                    , subscribePaymentsReq
-                    , PaymentsRes.class);
-
-            return response.getBody();
-        } catch (HttpClientErrorException e) {
-            log.error("Subscribe Rest OutBound Client Exception payments: {}", e.getMessage());
-            throw new HttpClientErrorException(e.getStatusCode(), "Client Exception");
-        } catch (HttpServerErrorException e) {
-            log.error("Subscribe Rest OutBound Server Exception payments: {}", e.getMessage());
-            throw new HttpServerErrorException(e.getStatusCode(), "Server Exception");
-        } catch (ResourceAccessException e) {
-            log.error("Subscribe Rest OutBound Resource Access Exception payments: {}", e.getMessage());
-            throw new ResourceAccessException("Subscribe Rest OutBound Resource Access Exception");
-        } catch (RuntimeException e) {
-            log.error("Subscribe Rest OutBound Runtime Exception payments: {}", e.getMessage());
-            throw new RuntimeException("Subscribe Rest OutBound Runtime Exception");
-        } catch (Exception e) {
-            log.error("Subscribe Rest OutBound Exception payments: {}", e.getMessage());
-            throw new RuntimeException("Subscribe Rest OutBound Exception");
-        }
+        return handleRequest(() -> subscribeHttpRequest(
+                subscribeUrlProperties.getPayments(),
+                uriVariables,
+                subscribePaymentsReq,
+                PaymentsRes.class));
     }
 
     @Override
-    public SubscribeExpireRes expire(
-            final Id<String> bid,
-            final SubscribeExpireReq subscribeExpireReq
-    ) {
-
+    public SubscribeExpireRes expire(final Id<String> bid, final SubscribeExpireReq subscribeExpireReq) {
         log.info("SubscribeRestTempleUseCase.expire");
-
         Map<String, Object> uriVariables = createUriVariables(bid);
-
-        try {
-            ResponseEntity<SubscribeExpireRes> postSubsExpire = restTemplate.postForEntity(
-                    domainUrlProperties.getApi() + subscribeUrlProperties.getExpire(),
-                    uriVariables,
-                    merchantProperties.getToken(),
-                    mapper.toJson(subscribeExpireReq),
-                    SubscribeExpireRes.class
-            );
-
-            return postSubsExpire.getBody();
-        } catch (HttpClientErrorException e) {
-            log.error("Subscribe Rest OutBound Client Exception expire: {}", e.getMessage());
-            throw new HttpClientErrorException(e.getStatusCode(), "Client Exception");
-        } catch (HttpServerErrorException e) {
-            log.error("Subscribe Rest OutBound Server Exception expire: {}", e.getMessage());
-            throw new HttpServerErrorException(e.getStatusCode(), "Server Exception");
-        } catch (ResourceAccessException e) {
-            log.error("Subscribe Rest OutBound Resource Access Exception expire: {}", e.getMessage());
-            throw new ResourceAccessException("Subscribe Rest OutBound Resource Access Exception");
-        } catch (RuntimeException e) {
-            log.error("Subscribe Rest OutBound Runtime Exception expire: {}", e.getMessage());
-            throw new RuntimeException("Subscribe Rest OutBound Runtime Exception");
-        } catch (Exception e) {
-            log.error("Subscribe Rest OutBound Exception expire: {}", e.getMessage());
-            throw new RuntimeException("Subscribe Rest OutBound Exception");
-        }
+        return handleRequest(() -> subscribeHttpRequest(
+                subscribeUrlProperties.getExpire(),
+                uriVariables,
+                subscribeExpireReq,
+                SubscribeExpireRes.class));
     }
 
     private Map<String, Object> createUriVariables(Id<String> bid) {
@@ -146,13 +72,36 @@ public class SubscribeCommandClient implements SubscribeOutboundPort {
         return uriVariables;
     }
 
-    private <T, R> ResponseEntity<R> subscribeHttpRequest(String payments, Map<String, Object> uriVariables, T request, Class<R> responseType) {
+    private <T, R> ResponseEntity<R> subscribeHttpRequest(String uri, Map<String, Object> uriVariables, T request, Class<R> responseType) {
         return restTemplate.postForEntity(
-                domainUrlProperties.getApi() + payments,
+                domainUrlProperties.getApi().concat(uri),
                 uriVariables,
                 merchantProperties.getToken(),
                 mapper.toJson(request),
                 responseType
         );
+    }
+
+    // 공통 예외 처리 메소드
+    private <T> T handleRequest(Supplier<ResponseEntity<T>> requestSupplier) {
+        try {
+            ResponseEntity<T> response = requestSupplier.get();
+            return response.getBody();
+        } catch (HttpClientErrorException e) {
+            log.error("Subscribe Rest OutBound Client Exception: {}", e.getMessage());
+            throw new HttpClientErrorException(e.getStatusCode(), "Client Exception");
+        } catch (HttpServerErrorException e) {
+            log.error("Subscribe Rest OutBound Server Exception: {}", e.getMessage());
+            throw new HttpServerErrorException(e.getStatusCode(), "Server Exception");
+        } catch (ResourceAccessException e) {
+            log.error("Subscribe Rest OutBound Resource Access Exception: {}", e.getMessage());
+            throw new ResourceAccessException("Subscribe Rest OutBound Resource Access Exception");
+        } catch (RuntimeException e) {
+            log.error("Subscribe Rest OutBound Runtime Exception: {}", e.getMessage());
+            throw new RuntimeException("Subscribe Rest OutBound Runtime Exception");
+        } catch (Exception e) {
+            log.error("Subscribe Rest OutBound Exception: {}", e.getMessage());
+            throw new RuntimeException("Subscribe Rest OutBound Exception");
+        }
     }
 }
